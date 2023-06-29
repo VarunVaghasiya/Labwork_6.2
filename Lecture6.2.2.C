223888db8e91d49bfdb27e164594e924018ead67
VarunVaghasiya <136708501+VarunVaghasiya@users.noreply.github.com>
2023-06-29 18:12:09 +0530

    Add files via upload

---
+++ Lecture6.2.2.C
@@ -0,0 +1,13 @@
+//Write a program to print 10 to 1 using for loop.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+int p;
+clrscr();
+   for(p=10;p>=1;p--){
+   printf("%d\n",p);
+}
+getch();
+}
